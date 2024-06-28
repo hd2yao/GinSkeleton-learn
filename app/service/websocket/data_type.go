1f5702852b137a63d46d12e9401233523c7e54be
hd2yao <1054290355@qq.com>
2024-06-28 14:39:22 +0800

    feat: 新增返回给小程序的消息结构

--- app/service/websocket/data_type.go
+++ app/service/websocket/data_type.go
@@ -7,3 +7,18 @@ type MsgPush struct {
 	Code int64 `json:"code"`
 	Data []msg.MsgContentModel
 }
+
+// ReturnClientMsg 接受到 ws 客户端发送过来的消息
+type ReturnClientMsg struct {
+	Code int64  `json:"code"`
+	Msg  string `json:"msg"`
+	Data struct {
+		Type               int64  `json:"type"`
+		UserId             int64  `json:"user_id"`
+		RoomId             int64  `json:"room_id"`
+		HandFree           int    `json:"hands_free"`
+		DeviceType         int    `json:"device_type"`
+		UserIdCallerTitle  string `json:"user_id_caller_title"`
+		UserIdCallerAvatar string `json:"user_id_caller_avatar"`
+	} `json:"data"`
+}
